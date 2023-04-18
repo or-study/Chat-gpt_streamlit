a2595abd928f729465ffdf56c3a3df0f670376bf
Derek <89770828+or-study@users.noreply.github.com>
2023-04-18 17:38:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from message_log import message_log
 
-api_key = "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"
+
 openai.api_key = api_key
 
 def generate_response(message_log):
